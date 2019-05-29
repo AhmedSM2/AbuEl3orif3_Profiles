cd92c2c53e1f47dbbe36004e417287cccf968b3a
A1 <ahmedshaaban837@gmail.com>
2019-05-30 01:08:48 +0200

    first commit profiles

---
+++ app/src/main/java/com/example/test/EditQuestionCommentActivity.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EditQuestionCommentActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_question_comment);
+    }
+}
